b577db2c32f9b05b53e41472b251292cc39436c6
hani.patel <hani.patel@intellective.co>
2018-08-08 12:17:56 -0400

    changes to app.js file

--- app.js
+++ app.js
@@ -3,16 +3,15 @@ var app = express();
 
 app.get('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Hello This is my first project to codestar'
   });
 });
 
 app.post('/', function(req, res) {
   res.send({
-    "Output": "Hello World!"
+    Output: 'Hello this is my first project to corestar'
   });
 });
 
-
 // Export your Express configuration so that it can be consumed by the Lambda handler
-module.exports = app
+module.exports = app;
